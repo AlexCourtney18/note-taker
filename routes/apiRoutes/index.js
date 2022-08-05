f8a0a5e23ccbb380a489ab599cd768182a803ba0
Alex <lex.courtney18@gmail.com>
2022-08-05 10:02:36 -0500

    get server online

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,33 @@
+const router = require('express').Router();
+const { filterByQuery, findById, createNewAnimal, validateAnimal } = require('../../lib/animals');
+const { animals } = require('../../data/animals');
+
+router.get('/notes', (req, res) => {
+  let results = animals;
+  if (req.query) {
+    results = filterByQuery(req.query, results);
+  }
+  res.json(results);
+});
+
+router.get('/animals/:id', (req, res) => {
+  const result = findById(req.params.id, animals);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
+});
+
+router.post('/notes', (req, res) => {
+  req.body.id = notes.length.toString();
+
+  if (!validateNote(req.body)) {
+    res.status(400).send('Error');
+  } else {
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+  }
+});
+
+module.exports = router;
