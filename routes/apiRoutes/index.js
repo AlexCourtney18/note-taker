8930c697bb56dbfc302d878081fe8b6673ab48ab
Alex <lex.courtney18@gmail.com>
2022-08-05 14:39:28 -0500

    push before logging off for the day

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,33 +1,6 @@
 const router = require('express').Router();
-const { createNewNote, validateNote } = require('../../lib/notes.js');
-const { notes } = require('../../db/db.json');
+const noteRoutes = require('../apiRoutes/noteRoutes');
 
-router.get('/notes', (req, res) => {
-  let results = notes;
-  if (req.query) {
-    return results;
-  }
-  res.json(results);
-});
-
-router.get('/notes/:id', (req, res) => {
-  const result = findById(req.params.id, notes);
-  if (result) {
-    res.json(result);
-  } else {
-    res.send(404);
-  }
-});
-
-router.post('/notes', (req, res) => {
-  req.body.id = notes.length.toString();
-
-  if (!validateNote(req.body)) {
-    res.status(400).send('Error');
-  } else {
-    const note = createNewNote(req.body, notes);
-    res.json(note);
-  }
-});
+router.use(noteRoutes);
 
 module.exports = router;
